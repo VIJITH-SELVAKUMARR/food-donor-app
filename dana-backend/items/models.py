e8883bdbf1f918f8e1a0348c165015e687c123d6
VIJITH-SELVAKUMARR <vijithcell1@gmail.com>
2025-09-11 22:32:13 +0530

    added dana/settings.py, dana/settings.py, Custom User model, Donations models, Serializers, Views & URLs, Admin registration

--- dana-backend/items/models.py
+++ dana-backend/items/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from donations.models import Donation
+
+class FoodItem(models.Model):
+    donation = models.ForeignKey(Donation, related_name='items', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    quantity = models.CharField(max_length=100)  # flexible: "2 boxes", "5 kg"
+    estimated_expiry_hours = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.quantity})"
+
 
 # Create your models here.
